1b3e79f4e3b8f7f79d13e789d9dbb035d76ff839
Sergey <C:\Users\Sergey\AppData\Roaming\The Bat!>
2018-08-25 18:05:13 +0300

    initial commit

---
+++ src/components/App/index.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-param-reassign */
+import React, {Component} from 'react';
+import styles from './styles.css'
+
+export default class App extends Component {
+  state = {
+  };
+
+  render() {
+
+    return (
+      <div className={styles.container}>
+        <h1>walkMe test task</h1>
+      </div>
+    );
+  }
+}
